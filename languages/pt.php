29f21d33339c177b151d11cefdf9bae9c0f734a0
Hiato <hiato@riseup.net>
2014-05-28 23:11:47 -0300

    Adicionada frase em português

---
+++ languages/pt.php
@@ -0,0 +1,10 @@
+<?php
+
+$portuguese = array(
+
+	'debaixo_theme:landing:title' => '¡Redes sociais do povo para o povo!',
+	'debaixo_theme:landing:subtitle' => 'Porque "as ferramentas do mestre nunca destruirão o império".',
+
+);
+
+add_translation("pt", $portuguese);
